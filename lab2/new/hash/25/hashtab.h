f47681c3df804da2c393636bf62cb85c25e32679
ProkopenkoRoman <long399@mail.ru>
2016-04-25 14:24:12 +0700

    Create hashtab.h

---
+++ lab2/new/hash/25/hashtab.h
@@ -0,0 +1,24 @@
+#ifndef HESH_TAB
+#define HESH_TAB
+
+#define HASHTAB_SIZE 900
+#define HASHTAB_MUL 31
+
+typedef struct list_node {
+	char *key;
+	int value;
+	struct list_node *next;
+} listnode;
+
+unsigned int hashtab_hash(char *key);
+void hashtab_init( listnode **hashtab);
+void hashtab_add(listnode **hashtab, char *key, int value);
+listnode *hashtab_lookup(listnode **hashtab, char *key);
+void hashtab_delete(listnode **hashtab, char *key);
+listnode *hashtab_add_lookup(listnode **hashtab, char *key);
+unsigned add_hash(char *key, int len);
+void hashtab_double_add(listnode **hashtab, char *key, int value);
+
+listnode *hashtab[HASHTAB_SIZE];
+
+#endif
